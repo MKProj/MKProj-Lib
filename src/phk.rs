19399a9eaad81856c2725d6633d9309f060ca4ea
mustafif0929@gmail.com <mustafif0929@gmail.com>
2021-04-12 21:09:01 -0400

    uupdate

--- src/phk.rs
+++ src/phk.rs
@@ -19,11 +19,11 @@ pub mod phaktionz {
     }
     pub fn fetch(sub_command: String, format: String) {
         //! Fetches different aspects in HTML, EPUB or PDF
-        let url = "https://github.com/MKProj/Phaktionz/raw/main/DOCS/".to_owned()
-            + &sub_command
-            + "."
-            + &format;
-        let wget = std::process::Command::new("wget").arg(url).output();
+        let url = format!(
+            "https://github.com/MKProj/Phaktionz/raw/main/DOCS/{}.{}",
+            sub_command, format
+        );
+        let wget = std::process::Command::new("wget").arg(url).spawn();
     }
     pub mod rules {
         //! The Rules Module is based on describing various aspects of the game
